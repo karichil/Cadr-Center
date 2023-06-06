76a71b33b5acbe72913cd36957e99eda8eb64ec7
karichil <karina.chilkiewicz@gmail.com>
2023-06-06 12:09:39 +0200

    Added Unit Tests Project

---
+++ CardCenterTests/BankTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace CardCenterTests;
+
+[TestFixture]
+public class BankTest
+{
+    [Test]
+    public void ABc()
+    {
+        var result = false;
+        Assert.AreEqual(true,result);
+    }
+}
